02a6f0fdca07776ab262daf6ab61d940d99da53e
Mohammed Fadin <mfadin@sa.ibm.com>
2024-10-11 14:14:57 +0400

    ADD Azure AI Search and update OpenAI to v1

--- function_app.py
+++ function_app.py
@@ -1,65 +1,104 @@
 import os
 import requests
 import json
-import openai
+from openai import AzureOpenAI
 import azure.functions as func
 import logging
 from dotenv import load_dotenv
 
+load_dotenv()
+
 app = func.FunctionApp()
 
 
-@app.function_name(name="yourfunction_trigger_name")
-@app.route(route="yourfunction_trigger_name", auth_level=func.AuthLevel.FUNCTION)
-def main(req: func.HttpRequest) -> func.HttpResponse:
+@app.function_name(name="chat")  # This is your function name, could be anything
+@app.route(
+    route="chat", auth_level=func.AuthLevel.FUNCTION
+)  # route is the endpoint of the function (e.g.https://<functionapp>.azurewebsites.net/api/chat)
+def main(
+    req: func.HttpRequest,
+) -> (
+    func.HttpResponse
+):  # if you change the main function name, make sure to change the entrypoint in function.json
     logging.info("Python HTTP trigger function processed a request.")
-    load_dotenv()
-    openai.api_key = os.getenv("AZURE_OPENAI_API_KEY")
-    openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
-    openai.api_type = "azure"
-    openai.api_version = "2023-05-15"
-    deployment_name = "gpt3-deployment"
 
-    # getting the parameters from the request
-    text_input = req.get_body().decode("utf-8")
-    print("Printing out the text from RSS feed")
-    print(text_input)
-    # Send a completion call to generate an answer
-    print("Sending a test completion job")
+    # OPTIONAL if you want to integrate Azure AI Search
+    aisearch_endpoint = os.getenv("AISEARCH_ENDPOINT")
+    aisearch_key = os.getenv("AISEARCH_KEY")
+    aisearch_index = os.getenv("AISEARCH_INDEX")
+    aisearch_resourcename = os.getenv("AISEARCH_RESOURCENAME")
 
-    # few shots example
-    role_system = """
-        Act as an experienced chatbot.....
-    """
-    few_shots_user_content = """
-        {
-            "message":"what do you think of blockchain?"
-        }
-    """
-    few_shots_assistant_response = """"
-        {
-            "response":"I think blockchain is a revolutionary technology that has the potential to change the way we do business. It's a decentralized, secure, and transparent way to store and transfer data. What do you think about it?"
-        }
-    """
-    response = openai.ChatCompletion.create(
-        engine=deployment_name,
-        response_format={"type": "json_object"},
+    client = AzureOpenAI(
+        azure_endpoint=os.getenv("OPENAI_API_BASE"),
+        api_key=os.getenv("OPEN_API_KEY"),
+        api_version=os.getenv("OPENAI_API_VERSION"),
+    )
+
+    # Getting the parameters from the request
+    user_input = req.get_body().decode("utf-8")
+    logging.info("POST request payload contains:{user_input}")
+
+    response = client.chat.completions.create(
+        model="hybridcopilot",  # model = "deployment_name".
         messages=[
-            {"role": "system", "content": role_system},
+            {
+                "role": "system",
+                "content": "You are an AI assistant that helps people find information.",
+            },
             {
                 "role": "user",
-                "content": few_shots_user_content,
+                "content": "hello",
             },
             {
                 "role": "assistant",
-                "content": few_shots_assistant_response,
+                "content": "Hello! How can I assist you today?",
             },
-            {"role": "user", "content": text_input},
+            {"role": "user", "content": "why am i paying warranty deposit?"},
+            {
+                "role": "assistant",
+                "content": "The warranty deposit is an amount you pay if you are sponsoring a dependent second degree, typically in humanitarian cases. It is important to keep the receipt showing that you paid the warranty deposit [doc1].",
+            },
+            {
+                "role": "user",
+                "content": user_input,
+            },
         ],
+        max_tokens=800,
+        temperature=0.7,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None,
+        stream=False,
+        extra_body={
+            "data_sources": [
+                {
+                    "type": "azure_search",
+                    "parameters": {
+                        "endpoint": f"{aisearch_endpoint}",
+                        "index_name": f"{aisearch_index}",
+                        "semantic_configuration": "azureml-default",
+                        "query_type": "vector_semantic_hybrid",
+                        "fields_mapping": {},
+                        "in_scope": True,
+                        "role_information": "You are an AI assistant that helps people find information.",
+                        "filter": None,
+                        "strictness": 3,
+                        "top_n_documents": 5,
+                        "authentication": {"type": "api_key", "key": f"{aisearch_key}"},
+                        "embedding_dependency": {
+                            "type": "deployment_name",
+                            "deployment_name": "textembeddingada",
+                        },
+                    },
+                }
+            ]
+        },
     )
-    post_json_body = response["choices"][0]["message"]["content"]
-    print(post_json_body)
+
+    model_response = response.choices[0].message.content
+    logging.info(model_response)
 
     return func.HttpResponse(
-        json.dumps(post_json_body), status_code=200, mimetype="application/json"
+        json.dumps(model_response), status_code=200, mimetype="application/json"
     )
